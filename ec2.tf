872e384ee0607964cf10bb4a1b50427618d29158
root <root@ip-172-31-83-42.ec2.internal>
2023-06-08 12:54:55 +0000

    TFfile

--- ec2.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_instance" "gaurav" {
-  ami                     = "ami-04a0ae173da5807d3"
-  instance_type           = "t2.micro"
-}
-
--- main.tf
+++ main.tf
@@ -4,3 +4,7 @@ provider "aws" {
   secret_key = "2+NHsKCDyGvHDOLefjYHsDzfsw+nESHFvu2N8irT"          #2+NHsKCDyGvHDOLefjYHsDzfsw+nESHFvu2N8irT
 }
 
+resource "aws_instance" "gaurav" {
+  ami                     = "ami-0715c1897453cabd1"
+  instance_type           = "t2.micro"
+}
